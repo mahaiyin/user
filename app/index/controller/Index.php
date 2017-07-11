86b6be0591e0d2c7fd8c1c6f748cf49feeeffb21
马海银 <mahaiyin@qq.com>
2017-07-12 05:20:24 +0800

    首页

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -5,6 +5,10 @@ class Index
 {
     public function index()
     {
-        return '123';
+        return view(null,[
+            'title' => '用户中心',
+            'keywords' => '用户中心',
+            'description' => '用户中心'
+        ]);
     }
 }
